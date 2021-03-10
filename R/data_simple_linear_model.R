07b8bed09579333bf5033ebd482b18f20d909660
Alex Knudson <aknudson@nevada.unr.edu>
2021-03-10 12:48:41 -0800

    add basic renv structure

---
+++ R/data_simple_linear_model.R
@@ -0,0 +1,9 @@
+set.seed(2020-03-10)
+
+n <- 100
+x <- rnorm(n)
+y <- rnorm(n, 3*x + 2)
+
+simple_linear_model <- data.frame(x = x, y = y)
+
+save(simple_linear_model, file = "data/simple_linear_model.rda")
